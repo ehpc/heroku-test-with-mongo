facc2df6a1646d68a880ab7d27ab360c8a59073c
ehpc <ehpc@ehpc.io>
2019-11-21 09:21:42 +0300

    first

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const path = require("path");
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// const mongoose = require("mongoose");
+// mongoose.connect("mongodb://localhost:27017/learnAuth", {
+//   useNewUrlParser: true
+// });
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// Обработка ошибок.
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+module.exports = app;
